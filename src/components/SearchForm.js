d2eb25f7c5a000371b8e096cf11430eeb3f3be59
finn-01 <nguyensam576@gmail.com>
2022-01-18 10:23:53 +0700

    context.js setup

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,6 +3,8 @@ import React from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
+	const { setSearchTerm } = useGlobalContext();
+
 	return <div>Hello Search Form</div>;
 };
 
