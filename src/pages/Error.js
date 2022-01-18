f5f87f8884f3f79b588dbb90d2e0b2e57a95c3b7
finn-01 <nguyensam576@gmail.com>
2022-01-18 09:44:13 +0700

    error component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Error = () => {
-	return <div>Hello Error</div>;
+	return (
+		<section className="error-page section">
+			<div className="error-container">
+				<h1>oops! it's a dead end</h1>
+				<Link to="/" className="btn btn-primary">
+					Back home
+				</Link>
+			</div>
+		</section>
+	);
 };
 
 export default Error;
